9a1e517ba1818cdf4766e6e3856d6eb3ff59305d
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:17:35 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,33 +1,29 @@
-output "federated_identity_credentials" {
-  description = "All federated_identity_credential resources"
-  value       = azurerm_federated_identity_credential.federated_identity_credentials
-}
 output "federated_identity_credentials_audience" {
-  description = "List of audience values across all federated_identity_credentials"
-  value       = [for k, v in azurerm_federated_identity_credential.federated_identity_credentials : v.audience]
+  description = "Map of audience values across all federated_identity_credentials, keyed the same as var.federated_identity_credentials"
+  value       = { for k, v in azurerm_federated_identity_credential.federated_identity_credentials : k => v.audience }
 }
 output "federated_identity_credentials_issuer" {
-  description = "List of issuer values across all federated_identity_credentials"
-  value       = [for k, v in azurerm_federated_identity_credential.federated_identity_credentials : v.issuer]
+  description = "Map of issuer values across all federated_identity_credentials, keyed the same as var.federated_identity_credentials"
+  value       = { for k, v in azurerm_federated_identity_credential.federated_identity_credentials : k => v.issuer }
 }
 output "federated_identity_credentials_name" {
-  description = "List of name values across all federated_identity_credentials"
-  value       = [for k, v in azurerm_federated_identity_credential.federated_identity_credentials : v.name]
+  description = "Map of name values across all federated_identity_credentials, keyed the same as var.federated_identity_credentials"
+  value       = { for k, v in azurerm_federated_identity_credential.federated_identity_credentials : k => v.name }
 }
 output "federated_identity_credentials_parent_id" {
-  description = "List of parent_id values across all federated_identity_credentials"
-  value       = [for k, v in azurerm_federated_identity_credential.federated_identity_credentials : v.parent_id]
+  description = "Map of parent_id values across all federated_identity_credentials, keyed the same as var.federated_identity_credentials"
+  value       = { for k, v in azurerm_federated_identity_credential.federated_identity_credentials : k => v.parent_id }
 }
 output "federated_identity_credentials_resource_group_name" {
-  description = "List of resource_group_name values across all federated_identity_credentials"
-  value       = [for k, v in azurerm_federated_identity_credential.federated_identity_credentials : v.resource_group_name]
+  description = "Map of resource_group_name values across all federated_identity_credentials, keyed the same as var.federated_identity_credentials"
+  value       = { for k, v in azurerm_federated_identity_credential.federated_identity_credentials : k => v.resource_group_name }
 }
 output "federated_identity_credentials_subject" {
-  description = "List of subject values across all federated_identity_credentials"
-  value       = [for k, v in azurerm_federated_identity_credential.federated_identity_credentials : v.subject]
+  description = "Map of subject values across all federated_identity_credentials, keyed the same as var.federated_identity_credentials"
+  value       = { for k, v in azurerm_federated_identity_credential.federated_identity_credentials : k => v.subject }
 }
 output "federated_identity_credentials_user_assigned_identity_id" {
-  description = "List of user_assigned_identity_id values across all federated_identity_credentials"
-  value       = [for k, v in azurerm_federated_identity_credential.federated_identity_credentials : v.user_assigned_identity_id]
+  description = "Map of user_assigned_identity_id values across all federated_identity_credentials, keyed the same as var.federated_identity_credentials"
+  value       = { for k, v in azurerm_federated_identity_credential.federated_identity_credentials : k => v.user_assigned_identity_id }
 }
 
